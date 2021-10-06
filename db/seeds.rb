b4a64cf66eaecb0e2478e7a9ed6a3a9f2cd0e08e
dat0800720 <damducdat0807@gmail.com>
2021-10-07 01:43:23 +0700

    add template par2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if User.none?
+  User.create!(
+    name: 'admin',
+    email: 'dat@gmail.com',
+    password: '123456',
+    password_confirmation: '123456',
+    user_type: 1,
+    status: 0
+  )
+
+
+  20.times do |n|
+    name = "staff-#{n+1}"
+    email = "staff-#{n+1}@railstutorial.org"
+    password = "123456"
+    User.create!(
+      name: name,
+      email: email,
+      password:password,
+      password_confirmation: password,
+      user_type: 0,
+      status: 0
+    )
+  end
+end
