f5d5a11be36daee356a15d56c2587e41aa12a446
dat0800720 <damducdat0807@gmail.com>
2021-12-18 03:21:57 +0700

    change_interface_and_complete_reques

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,16 @@
 class RequestsController < ApplicationController
-  before_action :find_request, only: [:show, :edit, :update, :destroy]
+  before_action :find_request, only: [:show, :edit, :approved, :update, :destroy]
   def index
     if params[:a] == "send"
-      @pagy, @requests = pagy(Request.where(member_id: current_user.member&.id), items: 10)
+      @pagy, @requests = pagy(Request.where(member_id: current_user.member&.id).where.not(request_status: 1), items: 10)
     elsif params[:a] == "approved"
       @pagy, @requests = pagy(Request.where.not(request_status: 1).joins(:member_requests).where(member_requests: { member_id: current_user.member&.id, type_recipient: 0}), items: 10)
     elsif params[:a] == "followed"
       @pagy, @requests = pagy(Request.where.not(request_status: 1).joins(:member_requests).where(member_requests: { member_id: current_user.member&.id, type_recipient: 1}), items: 10)
+    elsif params[:a] == "draft"
+      @pagy, @requests = pagy(Request.where(member_id: current_user.member&.id).where(request_status: 1), items: 10)
     else
-      @pagy, @requests = pagy(Request.where(member_id: current_user.member&.id), items: 10)
+      @pagy, @requests = pagy(Request.where(member_id: current_user.member&.id).where.not(request_status: 1), items: 10)
     end
   end
   
@@ -40,24 +42,30 @@ class RequestsController < ApplicationController
     end
   end
 
+  def approved
+    @request.request_status = 2
+    @request.save!
+    redirect_to request_path
+  end
+
   def show; end
 
   def destroy
-    request.destroy
+    @request.destroy
     flash[:success] = t("body.successfully")
-    redirect_to @requests
+    redirect_to @requests_url
   end
 
   private
   def find_request
-    @request = Request.find_by(id: params[:id])
+    @request = Request.includes.find_by(id: params[:id])
     unless @request
       redirect_to root_path
     end
   end
 
   def request_params
-    params.require(:request).permit(:content, :member_id, :holiday_id, :request_status, approver_ids: [], follower_ids: [],
+    params.require(:request).permit(:content, :member_id, :holiday_id, :request_status, :button_draft, approver_ids: [], follower_ids: [],
       break_times_attributes: [:id, :start_time, :end_time, :day, :_destroy])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def select_type select_i18n
+    I18n.t("#{select_i18n}").map{ |key, value| [value, key] }
+  end
 end
